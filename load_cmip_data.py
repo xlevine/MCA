d9ada064e29cfac512fee8b457e4934415e706d3
Xavier <61874728+xlevine@users.noreply.github.com>
2021-03-04 12:00:03 +0100

    Add files via upload

---
+++ load_cmip_data.py
@@ -0,0 +1,198 @@
+import sys, os
+import numpy as np
+from netCDF4 import Dataset
+from pylab import *
+from os.path import expanduser
+home = expanduser("~")
+
+###########################################################################
+# LISTS: Defines name of variables (following CMIP6 convention) and seasons 
+month_list = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+season_dict = {'winter':['dec','jan','feb'],'summer':['jun','jul','aug'],'spring':['mar','apr','may'],'fall':['sep','oct','nov']}
+
+varlist_Amon = ['clt','evspsbl','hurs','pr','rlds','rsds','rsut','ta','tasmin','ttrea','utendepfd','va','vurea','wurea','epfy','hfls','hursmin','prc','rlus','rsdt','sfcWind','tas','tos','ua','utrea','vas','vvrea','wvrea','epfz','hfss','huss','psl','rlut','rsus','siconca','tasmax','ts','uas','uurea','vtrea','wap','zg']
+varlist_Omon = ['msftyz', 'mlotst', 'evs','friver','hfds','mlotst','sos','vmo','ficeberg2d','rsntds','nshfls','downempfluxoce','downnoswfluxoce','downswfluxoce', 'thetao', 'so', 'tos', 'uo', 'vo']
+varlist_SImon = ['siage','sithick','sialb','siconc','sidmassevapsubl','sisnthick','sitemptop','sivol','sistrxdtop','downempfluxice','downnoswfluxice','downswfluxice','freshwater','siheco','sinudg','sstnudg','totdownfluxice','vfxice','vfxsnw']
+
+# METADATA: Contains name convention and data path of CMIP6. THIS MUST BE CHANGED FOR EACH USER. 
+metadata_dict = {'expt_name':'historical-CMIP6','expt':'a1st','version':'v20200120','expt_name_path':'ssp245','model_type':'EC-Earth3','expt_folder':"/esarchive/exp/ecearth/",'file_path':"/cmorfiles/ScenarioMIP/EC-Earth-Consortium/",'sim_config':'r7i1p1f1','year_ini':2015,'miss_val':1.e20}
+mask_dict = {'mask_path_ocean':"/esarchive/autosubmit/con_files/",'mask_name_ocean':'mask.regions.Ec3.2_O1L75.nc','mask_path_atmos':"/esarchive/exp/ecearth/constant/",'mask_name_atmos':'mask.regions.ifs_512x256.nc'}
+area_dict={'area_root_path':"",'file_gridcell_ocean':'areacello_fx_EC-Earth3_gn.nc','var_gridcell_ocean':'areacello','file_gridcell_atmos':'areacella_fx_EC-Earth3_gr.nc','var_gridcell_atmos':'areacella'}
+
+def define_regional_mask(varname,region):
+    grid_type = define_grid(varname)
+    if 'areacello' in grid_type:
+        path = mask_dict['mask_path_ocean']
+        region_mask_name = mask_dict['mask_name_ocean']
+    else:
+        path = mask_dict['mask_path_atmos']
+        region_mask_name = mask_dict['mask_name_atmos']
+
+    mask_path = path + region_mask_name
+    var_file = Dataset(mask_path, 'r')
+    mask_region_3D = var_file.variables[region]
+    mask_region_3D = np.asarray(mask_region_3D)
+
+    if 'areacello' in grid_type:
+        mask_region = np.squeeze(mask_region_3D[0,0,:,:])
+    else:
+        mask_region = mask_region_3D  
+    mask_region = np.asarray(mask_region)
+
+    return mask_region
+
+def load_input_var(varname1, season1, member_start, member_end):
+    nmemb=np.arange(member_start,member_end+1,1)
+    var1 = []
+    for memb in nmemb:
+        print(memb)
+        [var1_memb, lon1, lat1, plev1] = load_var(memb,varname1,season1)
+        var1.append(var1_memb)
+    var1 = np.asarray(var1)
+        
+    return var1, lon1, lat1
+
+def load_input_var_2D(varname1, season1, region1, member_start, member_end):
+    nmemb=np.arange(member_start,member_end+1,1)
+    region1 = define_regional_mask(varname1,region1)
+    region1[np.where(region1==0)]=np.nan
+    var1_2D = []
+    for memb in nmemb:
+        print(memb)
+        [var1_memb, lon1, lat1, lev1] = load_var(memb,varname1,season1)        
+        var1_memb = var1_memb * region1
+        var1_memb_2D = compute_zmean(var1_memb)                        
+        var1_2D.append(var1_memb_2D)
+    var1_2D = np.asarray(var1_2D)
+
+    return var1_2D, lev1, lat1
+
+def compute_zmean(var1):
+    var1_zmean = np.nanmean(var1,axis=-1)
+    
+    return var1_zmean
+
+def load_var(num_member, varname, month_name):    
+    if month_name in month_list:
+        [var, lon_2d, lat_2d, plev_2d] = read_var_month(num_member, varname, month_name)
+    else:
+        var_season = []
+        for month in season_dict[month_name]: 
+            [var_month, lon_2d, lat_2d, plev] = read_var_month(num_member, varname, month)
+            var_season.append(var_month)
+        var_season = np.asarray(var_season)
+        var = np.nanmean(var_season,axis=0)
+        
+    return var, lon_2d, lat_2d, plev
+
+def read_var_month(num_member, varname, month_name):
+    month_index = get_timestamp(month_name)
+    [var_year, lon_2d, lat_2d, plev] = read_var(num_member, varname)
+
+    if len(np.shape(var_year))==4:
+        var = np.squeeze(var_year[int(month_index),:,:,:])
+    else:
+        var = np.squeeze(var_year[int(month_index),:,:])
+
+    return var, lon_2d, lat_2d, plev
+
+def compute_cell_area(varname):
+    root_path=area_dict['area_root_path']
+    grid_type = define_grid(varname)
+    if 'areacello' in grid_type:
+        file_gridcell=root_path + area_dict['file_gridcell_ocean']
+        gridcell_file = Dataset(file_gridcell, 'r')
+        weight_area = gridcell_file.variables[area_dict['var_gridcell_ocean']]     
+    else:
+        file_gridcell=root_path + area_dict['file_gridcell_atmos']
+        gridcell_file = Dataset(file_gridcell, 'r')
+        weight_area = gridcell_file.variables[area_dict['var_gridcell_atmos']]     
+    
+    weight_area= np.asarray(weight_area)
+    weight_area[np.where(weight_area>=metadata_dict['miss_val'])]=np.nan
+    weight_area = np.divide(weight_area,np.nansum(weight_area)*np.ones(shape(weight_area)))
+
+    return weight_area
+
+def read_var(num_member, varname):
+    sim_array = get_filename(num_member, varname)
+    var = []; lev = []
+    for k in np.arange(0,len(sim_array),1):
+        var_file = Dataset(sim_array[k], 'r')
+        var_k = var_file.variables[varname]
+        var.extend(var_k)
+    var=np.asarray(np.squeeze(var))
+    try:
+        grid_type = var_file.variables[varname].cell_measures
+    except:
+        grid_type = 'areacello' 
+
+    if 'areacello' in grid_type:
+        lon_2d = var_file.variables['longitude']; lat_2d = var_file.variables['latitude']
+        lon_2d = np.asarray(lon_2d); lat_2d = np.asarray(lat_2d) 
+        lon_2d_pos = np.add(lon_2d,180.0*np.ones(shape(lon_2d)))
+        lon_2d[np.where(lon_2d<0)] = lon_2d_pos[np.where(lon_2d<0)]
+        if len(np.shape(var))==4:
+            lev = var_file.variables['lev'][:]
+    else:
+        lon = var_file.variables['lon']; lat = var_file.variables['lat']
+        lon_2d = np.matlib.repmat(lon,len(lat),1); lat_2d_T = np.matlib.repmat(lat,len(lon),1)
+        lat_2d = lat_2d_T.T
+        lon_2d = np.asarray(lon_2d); lat_2d = np.asarray(lat_2d) 
+        if len(np.shape(var))==4:
+            lev = var_file.variables['plev'][:]
+
+    var_file.close()
+    var[np.where(var>=metadata_dict['miss_val'])] = np.nan
+    return var, lon_2d, lat_2d, lev
+
+def var_labels(varname):
+    sim_array = get_filename(0, varname)
+    var_file = Dataset(sim_array[0], 'r')
+    units = var_file.variables[varname].units
+    long_name = var_file.variables[varname].long_name
+    var_dic={'units':units,'long_name':long_name}
+
+    return var_dic
+
+def define_grid(varname):
+    sim_array = get_filename(0, varname)
+    var_file = Dataset(sim_array[0], 'r')
+    try:
+        grid_type = var_file.variables[varname].cell_measures
+    except:
+        grid_type = 'areacello'
+
+    return grid_type
+
+def get_filename(num_member, varname):
+    if varname in varlist_Omon:
+        domain='Omon'; sim_type='gn'
+    elif varname in varlist_SImon:
+        domain='SImon'; sim_type='gn'
+    elif varname in varlist_Amon:
+        domain='Amon'; sim_type='gr'
+
+    expt=metadata_dict['expt']; version=metadata_dict['version']; expt_name_path=metadata_dict['expt_name_path']; model_type=metadata_dict['model_type']; year_ini=metadata_dict['year_ini']; expt_folder=metadata_dict['expt_folder']; file_path=metadata_dict['file_path']; sim_config=metadata_dict['sim_config']
+
+    slash="/"; udscr='_'; dash='-'; end_file_nc='.nc'
+    year_membr = year_ini + num_member; date_year = [str(year_membr) + '01' + dash + str(year_membr) + '12']
+    varfile_path = file_path + model_type + slash
+    varname_expt_root = varname + udscr + domain + udscr + model_type + udscr + expt_name_path + udscr + sim_config + udscr + sim_type
+    root_path = expt_folder + expt + varfile_path + expt_name_path + slash + sim_config + slash + domain + slash + varname + slash + sim_type + slash + version
+
+    sim_array = []
+    for k in np.arange(0,len(date_year),1):
+        varname_expt_file = varname_expt_root + udscr + date_year[k] + end_file_nc        
+        sim = root_path + slash + varname_expt_file
+        sim_array.append(sim)        
+    sim_array = np.asarray(sim_array)
+
+    return sim_array
+
+def get_timestamp(month_name):
+    month_l = [i for i, s in enumerate(month_list) if month_name in s]
+    month = month_l[0] + 1
+    month_index = int(month) - 1
+
+    return month_index
---
+++ pca_2D_cmip_data.py
@@ -0,0 +1,209 @@
+import sys, os
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.matlib
+from mpl_toolkits.basemap import Basemap 
+from matplotlib.offsetbox import AnchoredText
+from pylab import *
+from os.path import expanduser
+from load_cmip_data import load_input_var_2D, define_regional_mask, var_labels, compute_cell_area
+from toolbox_cmip_data import convert_varglobal2D_to_vardomain1D, convert_vardomain1D_to_varglobal2D
+from svd_cmip_data import pca_1D, return_output
+home = expanduser("~")
+
+# Principal Component analysis (PCA) is powerful tool to assess the dominant modes of climatic variability; PCA determines the spatial pattern of a climatological variable (e.g. precipitation, sea surface temperature) that can explain the largest fraction of its spatio-temporal variance (e.g. interannual covariance), globally or over a specific region. This is done by performing a singular value decomposition (SVD) on the covariance matrix of the variable. A PCA returns a left singular vectors, which correspond to the spatial pattern of the leading modes of variability. Associated with each Leading Mode is a timeseries (the eigenvalues). 
+
+# Inputs of PCA: X_1[p,y,t], lat1[p,y], lev1[p,y], areas1[p,y], region1[p,y]
+# Outputs of PCA: E1_N[p,y]; P_1[t] (for each leading mode)
+
+###########################################################################
+# METADATA: Contains name convention and data path of CMIP6. THIS MUST BE CHANGED FOR EACH USER. 
+metadata_dict = {'expt_name':'historical-CMIP6','expt':'a1st','version':'v20200120','expt_name_path':'ssp245','model_type':'EC-Earth3','expt_folder':"/esarchive/exp/ecearth/",'file_path':"/cmorfiles/ScenarioMIP/EC-Earth-Consortium/",'sim_config':'r7i1p1f1','year_ini':2015,'miss_val':1.e20}
+# INPUT: User defines the input of the MCA: variable name, season and region for each variable of the covariance pair
+input_dict={'varname1':'ua','season1':'winter','region1':'Northern_Hemisphere','year_start':2015,'year_end':2020}
+
+###################################################### I: Master Script ######################################################
+def main():
+    # P1: Initializing data
+    [dataset1,surface_area1,mask_region1,lev1,lat1] = define_pca_2D_inputs()
+    # P2: Performing MCA
+    [varnorm,var1_modes_patterns,var1_modes_timeseries] = compute_pca(dataset1,surface_area1,mask_region1,lev1,lat1)
+    # P3: Plotting
+    var1_clim = np.nanmean(dataset1,axis=0)
+    plot_pca_2D(varnorm,var1_modes_patterns,var1_modes_timeseries,lev1,lat1,var1_clim)
+
+###################################################### II: Main Dependence  ######################################################
+## P1: Initializing data 
+def define_pca_2D_inputs():
+    year_ini = metadata_dict['year_ini']
+    varname1 = input_dict['varname1']; season1 = input_dict['season1']; region1 = input_dict['region1']; year_start = input_dict['year_start']; year_end = input_dict['year_end']
+
+    member_start = year_start - year_ini; member_end = year_end - year_ini
+    ### Define 2D surface area mask matrix (NA ocean)
+    [var1_2D,lev1,lat1] = load_input_var_2D(varname1,season1,region1,member_start,member_end) 
+    area1 = compute_cell_area(varname1)
+    region1 = define_regional_mask(varname1,region1)
+    [lat1_2D, lev1_2D] = convert_lev2D(lat1,lev1)
+
+    [area1_2D, region1_2D] = compute_2D_var(area1, region1, lev1)
+    print(np.shape(var1_2D), np.shape(area1_2D), np.shape(region1_2D), np.shape(lat1_2D), np.shape(lev1_2D))
+
+    return var1_2D, area1_2D, region1_2D, lev1_2D, lat1_2D
+
+#######
+## P2: Performing MCA
+def compute_pca(dataset1,area1,region1,lon1,lat1):
+    # load 2-D yearly timeseries (x,y,t)
+    ntime = shape(dataset1)[0]; nlon1 = shape(dataset1)[1]; nlat1 = shape(dataset1)[2] 
+
+    var1_yearseries_2D = np.nan * ones((ntime,nlon1,nlat1)); var1_yearseries_2D = dataset1
+
+    #### Convert from 2D to 1D and weight input by surface area mask matrix
+    [var1_yearseries_domain_1D,var1_yearseries_fulldomain_1D,var1_total_mask] = convert_varglobal2D_to_vardomain1D(var1_yearseries_2D,area1,region1,lon1,lat1)
+
+    [u, v, varnorm] = pca_1D(var1_yearseries_domain_1D)
+    [u_2D, u_fulldomain_1D] = convert_vardomain1D_to_varglobal2D(u,var1_total_mask,area1)
+
+    [var1_modes_timeseries,var1_modes_patterns] = return_output(u_2D,u_fulldomain_1D,u,var1_yearseries_fulldomain_1D)
+
+    return varnorm,var1_modes_patterns,var1_modes_timeseries
+
+## P3: Plotting
+def plot_pca_2D(varnorm,var1_modes_patterns,var1_modes_timeseries,lev1_2d,lat1_2d,var1_clim):
+
+    lev1_2d = np.multiply(lev1_2d,1.00000*np.ones(shape(lev1_2d))); lat1_2d = np.multiply(lat1_2d,1.00000*np.ones(shape(lat1_2d)))
+    varname1 = input_dict['varname1']; season1 = input_dict['season1']; region1 = input_dict['region1']
+    year_start = input_dict['year_start']; year_end = input_dict['year_end']
+
+    var1_dic = var_labels(varname1)
+
+    ### PLOT MODE PATTERNS AND TIME-SERIES
+    N_trunc=2; frac_cticks = 0.6
+    for N in np.arange(0,N_trunc,1):
+        print('Leading mode ' +  str(N+1) + ' explains ' + str(int(varnorm[N,N]*100))  + '%' + ' of variance')
+        var1_mode_patterns = np.squeeze(var1_modes_patterns[N,:,:])  
+        var1_mode_timeseries = np.squeeze(var1_modes_timeseries[N,:])
+        min_var1 = -np.nanmax(var1_modes_patterns)
+        cticks_1 = np.arange(frac_cticks*min_var1,-frac_cticks*min_var1*(.99+2./6),-frac_cticks*2.*min_var1/6)
+
+        # Plot left pattern
+        title_name = 'Mode ' + str(int(N+1)) + ' Pattern for ' + var1_dic['long_name']  + ' in ' + season1 + ' (' + str(int(varnorm[N,N]*100))  + '%' + ' VAR) ' + '[' + var1_dic['units'] + ']' 
+        figure_name = 'PCA' + str(int(N+1)) + '_pattern_' + varname1 + '_in_' + season1 + '_over_' + region1 + '_Y' + str(year_start) + 'Y' + str(year_end)
+        plot_2D_contour(lev1_2d,lat1_2d,var1_mode_patterns,cticks_1,var1_clim,title_name,figure_name)
+
+        # Plot left time series
+        title_name = 'Mode ' + str(int(N+1)) + ' Time-Series for ' + var1_dic['long_name'] + ' in ' + season1 + ' (' + str(int(varnorm[N,N]*100))  + '%' + ' VAR)'
+        figure_name = 'PCA' + str(int(N+1)) + '_series_' + varname1 + '_in_' + season1 + '_over_' + region1 + '_Y' + str(year_start) + 'Y' + str(year_end)
+        plot_series(var1_mode_timeseries,year_start,year_end,title_name,figure_name)
+
+############################################ III: Toolbox ##########################################################
+### P1: Processing zonal-mean data 
+def compute_2D_var(area1, region1, lev1):
+    nlev = len(lev1); nlat = np.shape(area1)[0]
+    area1_zmean = np.nanmean(area1,axis=-1) 
+    region1_zmean = np.nanmean(region1,axis=-1) # region1 = 0 / 1
+    region1_2D = []; area1_2D = []
+    for k in np.arange(0,nlev,1):
+        area1_2D.append(area1_zmean)
+        region1_2D.append(region1_zmean)
+    area1_2D = np.asarray(area1_2D)    
+    region1_2D = np.asarray(region1_2D)
+
+    dlev1 = delta_plev(lev1)
+    dlev1_2D = np.matlib.repmat(dlev1,nlat,1).T    
+    area1_2D = area1_2D * dlev1_2D
+
+    return area1_2D, region1_2D
+
+def convert_lev2D(lat,lev):
+    nlat = np.shape(lat)[0]; nlev = len(lev)
+    lat_2D = []; lev_2D = []
+    for k in np.arange(0,nlev,1):
+        lat_k = lat[:,0]
+        lev_k = []
+        for j in np.arange(0,nlat,1):
+            lev_k.append(lev[k])
+        lat_2D.append(lat_k)
+        lev_2D.append(lev_k)
+    lat_2D = np.asarray(lat_2D)
+    lev_2D = np.asarray(lev_2D)
+    
+    return lat_2D, lev_2D
+
+def delta_plev(plev):
+    # define difference
+    plev_f = np.ones(len(plev)+1)
+    plev_f[1:-1] = np.add(plev[0:-1],plev[1:]) / 2.0
+    plev_f[0] = 102450.0 
+    plev_f[-1] = 0 
+    dplev = diff(plev_f)
+
+    return dplev
+
+### P3: Plotting
+def plot_2D_contour(lev_2d,lat_2d,var_2d,color_ticks,var_clim,title,savefigure_name):
+
+    lev_2d = lev_2d / 100
+    plt.rcParams.update({'font.size': 17})
+    fwidth=8; fheight=8; margin = 0.6
+    left_margin  =  1.0 / fwidth; 
+    right_margin =  0.3 / fwidth;
+    bottom_margin =  margin / fheight; 
+    top_margin = margin / fheight
+    l = left_margin    # horiz. position of bottom-left corner
+    r = bottom_margin  # vert. position of bottom-left corner
+    w = 1 - (left_margin + right_margin) # width of axes
+    h = 1 - (bottom_margin + top_margin) # height of axes
+    fig = plt.figure(figsize=(fwidth, fheight))
+    ax = fig.add_axes([l, r, w, h])
+
+    ######## anomalies
+    color_map = 'RdBu_r'
+    cs = plt.pcolormesh(lat_2d, lev_2d, var_2d, cmap=color_map, vmin=min(color_ticks), vmax=max(color_ticks))
+    cbar = plt.colorbar(cs, ticks=color_ticks,pad=0.01)
+    plt.clim(min(color_ticks),max(color_ticks))
+
+    ######## climatology
+    max_val = np.nanmax([np.nanmax(var_clim),-np.nanmin(var_clim)])
+    nval =  2.*max_val/24
+    levels_p = np.arange(nval, max_val, nval)
+    levels_n = -levels_p[::-1]
+    plt.contour(lat_2d, lev_2d, var_clim, levels=levels_n, linestyles='dashed', linewidths=1.0, colors='k') #, alpha=0)
+    plt.contour(lat_2d, lev_2d, var_clim, levels=levels_p, linestyles='solid', linewidths=2.0, colors='k') # , alpha=0)
+
+    color_ticks_label = []
+    nticks = len(color_ticks)
+    for n in np.arange(0,nticks,1):
+        if (n % 2) == 0: 
+            color_ticks_label.append(str(round(color_ticks[n],2)))
+        else:
+            color_ticks_label.append('') 
+    print(color_ticks_label)    
+    cbar.ax.set_yticklabels(color_ticks_label)
+    plt.xlim((-30, 90)) 
+    lat_label = ['-30$^{\circ}$', '', '0$^{\circ}$', '', '30$^{\circ}$', '', '60$^{\circ}$', '', '90$^{\circ}$']
+
+    xlabel('Latitude')
+    ylabel('Pressure Level [hPa]')
+    plt.xticks(np.arange(-30, 105, 15), lat_label)
+    plt.gca().invert_yaxis()
+    plt.title(title, fontsize=8)
+    save_file = savefigure_name + '.png'
+    fig.savefig(save_file) #,op                                                                                 
+    plt.close()
+
+def plot_series(var_1d_yearseries,year_start,year_end,title,savefile_name):
+    year_duration = shape(var_1d_yearseries)[0]; year_label = []; year_step = 20
+    for t in np.arange(year_start, year_end+year_step, year_step):
+        year_label.append(str(t))           
+    fig = plt.figure()                                                          
+    plt.plot(np.arange(1, year_duration+1, 1), var_1d_yearseries, 'r')
+    plt.plot(np.arange(1, year_duration+1, 1), var_1d_yearseries, 'ro')
+    plt.plot(np.arange(1, year_duration+1, 1), np.zeros(shape(var_1d_yearseries)), 'k--')
+    plt.xticks(np.arange(1, year_duration+year_step, year_step), year_label)
+    plt.xlabel('Year')
+    plt.title(title, fontsize=8)
+    plt.show()
+    fig.savefig(savefile_name+'.png') #,optimize=True,quality=85)
+    plt.close()
+############## EOF ##############
